25876d4283d15d8ad8e2426aa5e5c834bb9cf57e
vito <vitopiegari01@gmail.com>
2019-12-22 11:20:13 +0100

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BookTracker());
+
+class BookTracker extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("ok"),
+        ),
+      ),
+    );
+  }
+}
