21067ce14906ab8d10cc6197fd15c0ac147de0c2
vito <vitopiegari01@gmail.com>
2020-03-08 09:20:55 +0100

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,35 @@
+import 'package:book_tracker/routes.dart';
+import 'package:book_tracker/theme/style_dark.dart' as dark_theme;
+import 'package:book_tracker/theme/style.dart' as light_theme;
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(BookTracker());
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(
+    BookTracker(),
+  );
+}
 
 class BookTracker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("ok"),
-        ),
-      ),
+      theme: light_theme.theme,
+      darkTheme: dark_theme.theme,
+      themeMode: ThemeMode.light, // testing, defaults to .system
+      supportedLocales: <Locale>[
+        Locale('en'),
+        Locale('it'),
+      ],
+      localizationsDelegates: <LocalizationsDelegate>[
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate
+      ],
+      routes: routes,
     );
   }
 }
