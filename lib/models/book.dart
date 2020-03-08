4b7bdf0f518d4229e0e3646f9eee5e312d3e4fc5
vito <vitopiegari01@gmail.com>
2020-03-08 09:26:41 +0100

    Updated book model with database utilities

--- lib/models/book.dart
+++ lib/models/book.dart
@@ -1,6 +1,8 @@
 import 'package:intl/intl.dart';
 
 class Book {
+  final String id;
+
   final String title;
 
   final List authors;
@@ -20,6 +22,7 @@ class Book {
   final String thumbnail;
 
   Book({
+    this.id,
     this.title,
     this.authors,
     this.description,
@@ -31,8 +34,9 @@ class Book {
     this.thumbnail,
   });
 
-  Book.fromJson(Map json)
-      : title = json["volumeInfo"]["title"],
+  Book.fromJson(Map<String, dynamic> json)
+      : id = json["id"],
+        title = json["volumeInfo"]["title"],
         authors = json["volumeInfo"]["authors"],
         description = json["volumeInfo"]["description"],
         publishedDate = json["volumeInfo"]["publishedDate"] == null
@@ -43,4 +47,32 @@ class Book {
         averageRating = json["volumeInfo"]["averageRating"],
         ratingsCount = json["volumeInfo"]["ratingsCount"],
         thumbnail = json["volumeInfo"]["imageLinks"]["thumbnail"];
+
+  Book.fromDatabaseJson(Map<String, dynamic> json)
+      : id = json["id"],
+        title = json["title"],
+        authors = (json["authors"] as String).split("|"),
+        publishedDate =
+            DateTime.fromMillisecondsSinceEpoch(json["publishedDateTimestamp"]),
+        description = json["description"],
+        pageCount = json["pageCount"],
+        averageRating = json["averageRating"],
+        categories = (json["categories"] as String).split("|"),
+        ratingsCount = json["ratingsCount"],
+        thumbnail = json["text"];
+
+  Map<String, dynamic> toDatabaseJson() {
+    return {
+      "id": id,
+      "title": title,
+      "authors": authors ?? [].join("|"),
+      "publishedDateTimestamp": publishedDate?.millisecondsSinceEpoch ?? 0,
+      "description": description,
+      "pageCount": pageCount,
+      "averageRating": averageRating,
+      "categories": categories ?? [].join("|"),
+      "ratingsCount": ratingsCount,
+      "thumbnail": thumbnail,
+    };
+  }
 }
